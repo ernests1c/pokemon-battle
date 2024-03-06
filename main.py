210704b00c5861a0a0d12d3ef1294cbe61df55fe
ernests1c <149561992+ernests1c@users.noreply.github.com>
2024-03-06 10:19:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -36,15 +36,51 @@ while True:
     choice = input("Enter your choice (1-5): ")
 
     if choice == '1':
+        pokemon_index = int(input("Which pokemon would you like to see?"))
+        print(pokemons[pokemon_index])
         # https://www.w3schools.com/python/python_dictionaries_access.asp
         pass
     elif choice == '2':
+        def sorting(pokemons_strong):
+            return int(pokemons_strong["total"])
+        pokemons.sort(key = sorting, reverse = True)
+        print(pokemons[0:10])
+
         # https://www.w3schools.com/python/python_lists_sort.asp
         pass
     elif choice == '3':
+        def sorting(pokemons_weak):
+            return int(pokemons_weak["total"])
+        pokemons.sort(key = sorting)
+        print(pokemons[0:10])
         # https://www.w3schools.com/python/python_lists_sort.asp
         pass
     elif choice == '4':
+        computer_choice = random.randint(1,43)
+        player_choice = int(input("Which pokemon would you like to choose?"))
+        player_health = int(pokemons[player_choice].get("total"))
+        computer_health = int(pokemons[computer_choice].get("total"))
+        while player_health > 0 and computer_health > 0:
+            player_damage = pokemons[player_choice].get("attack") - pokemons[computer_choice].get("defense") + random.randint(5,20)
+            computer_damage = pokemons[computer_choice].get("attack") - pokemons[player_choice].get("defense") + random.randint(5,20)
+            player_health = player_health - computer_damage
+            computer_health = computer_health - player_damage
+            print("Computer health left: ", computer_health)
+            print("Player health left: ", player_health)
+
+
+        
+        
+
+        
+        
+        
+
+        
+
+            
+            
+
         # Battle
         # 
         # https://www.w3schools.com/python/ref_random_choice.asp - random choice
